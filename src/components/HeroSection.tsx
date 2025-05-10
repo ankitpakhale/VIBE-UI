c2671e6056427208cd05e2950f5638aa10254c21
Ankit Pakhale <akp3067@gmail.com>
2025-05-10 22:00:12 +0530

    - Add simplex-noise dependency to package.json. - Integrate addVariablesForColors plugin in tailwind.config.ts. - Include HeroSection in Home component. - Build HeroSection using TextGenerateEffect, MovingBorder, WavyBackground, title, and description text. - Add TextGenerateEffect component to UI. - Add Spotlight component to UI. - Add MovingBorder component to UI. - Add WavyBackground component to UI.

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import { Spotlight } from "./ui/Spotlight";
+import { cn } from "@/utils/cn";
+import { Button } from "./ui/moving-border";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
+import { WavyBackground } from "./ui/wavy-background";
+
+function HeroSection() {
+  const heroSectionDescription =
+    "Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.";
+  return (
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col item-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+      <div className="relative flex h-[40rem] w-full overflow-hidden rounded-md bg-black/[0.96] antialiased md:items-center md:justify-center">
+        <WavyBackground className="max-w-4xl mx-auto">
+          {/* Background Grid */}
+          {/* <div
+            className={cn(
+              "pointer-events-none absolute inset-0 [background-size:40px_40px] select-none",
+              "[background-image:linear-gradient(to_right,#171717_1px,transparent_1px),linear-gradient(to_bottom,#171717_1px,transparent_1px)]"
+            )}
+          /> */}
+          <div className="p-4 relative z-10 w-full text-center">
+            {/* <Spotlight
+              className="-top-40 left-0 md:-top-20 md:left-60"
+              fill="red"
+            /> */}
+            <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+              Master the Art of Music
+            </h1>
+            <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+              <TextGenerateEffect words={heroSectionDescription} />
+            </p>
+            <div className="my-8">
+              <Button
+                borderRadius="1.75rem"
+                className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+              >
+                <Link href={"/courses"}>Explore Courses</Link>
+              </Button>
+            </div>
+          </div>
+        </WavyBackground>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
